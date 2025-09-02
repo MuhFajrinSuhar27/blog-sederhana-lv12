fa8673ffddf329ce90f005d130b0571daf6b75a1
unknown <fajrinsuhar97@gmail.com>
2025-09-02 14:55:18 +0800

    feat: menambahkan single post

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,18 +2,16 @@
   
 
 
-    @foreach ( $posts as $post )
     <article class="py-8 max-w-screen-md border-gray-300">
-        <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
             <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
         </a>
         <div class="text-base text-gray-500">
             <a href="#">{{ $post['author'] }}</a> | {{ $post['release'] }}
         </div>
-        <p class="my-4 font-light">{{ Str::limit( $post['body'] , 160)}}</p>
-        <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read More &raquo;</a>
+        <p class="my-4 font-light">{{( $post['body'])}}</p>
+        <a href="/posts" class="font-medium text-blue-500 hover:underline"> &laquo; Back to all posts</a>
     </article>
-    @endforeach
+ 
 
 </x-layout> 
     
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Arr;
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -10,25 +11,21 @@ Route::get('/', function () {
     return view('home', ['title' =>'Homepage']);
 });
 
-Route::get('/about', function () {
-    return view('about', ['title' => 'About']);
-});
-
-Route::get('/contact', function () {
-    return view('contact', ['title' => 'Contact']);
-});
 
 Route::get('/posts', function () {
 
     $posts = [
         [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
             'title' => 'Judul Artikel 1',
             'release' => '1 January',
             'author' => 'Muh. Fajrin Suhar',
             'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni fugiat error praesentium maxime vitae aspernatur cumque laudantium ut voluptatum non consectetur aliquam labore provident quasi nostrum perspiciatis commodi, mollitia iste?'
         ], 
-
         [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
             'title' => 'Judul Artikel 2',
             'release' => '2 January', 
             'author' => 'Muh. Fajrin Suhar',
@@ -37,6 +34,46 @@ Route::get('/posts', function () {
         ];
 
     return view('posts', ['title' => 'Posts' , 'posts' => $posts]);
+});
+
+Route::get('/posts/{slug}', function($slug){
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'release' => '1 January',
+            'author' => 'Muh. Fajrin Suhar',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni fugiat error praesentium maxime vitae aspernatur cumque laudantium ut voluptatum non consectetur aliquam labore provident quasi nostrum perspiciatis commodi, mollitia iste?'
+        ], 
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'release' => '2 January', 
+            'author' => 'Muh. Fajrin Suhar',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid corporis voluptate, atque ex nobis voluptatum, odit totam doloremque consectetur libero sapiente ipsum, quod numquam recusandae qui voluptatibus deleniti molestias perspiciatis'
+        ]
+        ];
+
+        
+        $post = Arr::first($posts, function($post) use ($slug){
+            return $post['slug'] == $slug;         
+        });
+        if(!$post ) abort(404);
+
+        return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
+
+
+
+Route::get('/about', function () {
+    return view('about', ['title' => 'About']);
+});
+
+Route::get('/contact', function () {
+    return view('contact', ['title' => 'Contact']);
 });
 
 
