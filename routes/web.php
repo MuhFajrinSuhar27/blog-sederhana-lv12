8d7c619c2d97b81b53339eb2800f542f9f8a037c
unknown <fajrinsuhar97@gmail.com>
2025-09-02 20:16:14 +0800

    fix: memperbaiki model single post

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+
+
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Arr;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+
 
 Route::get('/', function () {
     return view('home', ['title' =>'Homepage']);
@@ -13,59 +16,18 @@ Route::get('/', function () {
 
 
 Route::get('/posts', function () {
-
-    $posts = [
-        [
-            'id' => 1,
-            'slug' => 'judul-artikel-1',
-            'title' => 'Judul Artikel 1',
-            'release' => '1 January',
-            'author' => 'Muh. Fajrin Suhar',
-            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni fugiat error praesentium maxime vitae aspernatur cumque laudantium ut voluptatum non consectetur aliquam labore provident quasi nostrum perspiciatis commodi, mollitia iste?'
-        ], 
-        [
-            'id' => 2,
-            'slug' => 'judul-artikel-2',
-            'title' => 'Judul Artikel 2',
-            'release' => '2 January', 
-            'author' => 'Muh. Fajrin Suhar',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid corporis voluptate, atque ex nobis voluptatum, odit totam doloremque consectetur libero sapiente ipsum, quod numquam recusandae qui voluptatibus deleniti molestias perspiciatis'
-        ]
-        ];
+    $posts = Post::all();
 
     return view('posts', ['title' => 'Posts' , 'posts' => $posts]);
 });
 
+
 Route::get('/posts/{slug}', function($slug){
-    $posts = [
-        [
-            'id' => 1,
-            'slug' => 'judul-artikel-1',
-            'title' => 'Judul Artikel 1',
-            'release' => '1 January',
-            'author' => 'Muh. Fajrin Suhar',
-            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni fugiat error praesentium maxime vitae aspernatur cumque laudantium ut voluptatum non consectetur aliquam labore provident quasi nostrum perspiciatis commodi, mollitia iste?'
-        ], 
-        [
-            'id' => 2,
-            'slug' => 'judul-artikel-2',
-            'title' => 'Judul Artikel 2',
-            'release' => '2 January', 
-            'author' => 'Muh. Fajrin Suhar',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid corporis voluptate, atque ex nobis voluptatum, odit totam doloremque consectetur libero sapiente ipsum, quod numquam recusandae qui voluptatibus deleniti molestias perspiciatis'
-        ]
-        ];
+   
+    $post = Post::find($slug);
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 
-        
-        $post = Arr::first($posts, function($post) use ($slug){
-            return $post['slug'] == $slug;         
-        });
-        if(!$post ) abort(404);
-
-        return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
-
-
 
 
 Route::get('/about', function () {
